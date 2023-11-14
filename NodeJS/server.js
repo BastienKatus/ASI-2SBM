219834dcf688a0cad40550b00a3cd47d21d4547e
Simon MACHADO <simon.machado@tpg15.cpe.lan>
2023-11-14 12:11:52 +0100

    chat interactif

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -2,27 +2,48 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const axios = require('axios'); // Module pour effectuer des requêtes HTTP
+const axios = require('axios');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); // Vous pouvez créer ce fichier HTML pour l'interface utilisateur.
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('Utilisateur connecté');
+  console.log('Utilisateur connecté' + socket.id);
 
-  // Récupération de la liste des utilisateurs via l'API Spring Boot
+  // Récupération de la liste des utilisateurs via l'API Users (backend springboot)
   axios.get('http://localhost:8080/users')
     .then((response) => {
-      const users = response.data; // Liste des utilisateurs
-      socket.emit('userList', users); // Émettre la liste des utilisateurs au client
+      const users = response.data;
+      socket.emit('userList', users); // Émettre au client
     })
     .catch((error) => {
       console.error('Erreur lors de la récupération des utilisateurs depuis l\'API Spring Boot:', error);
     });
 
   socket.on('message', (message) => {
-    io.emit('message', message); // Diffusez le message à tous les utilisateurs connectés.
+    io.emit('message', message); // Broadcast
+  });
+
+  function getRoomName(user1, user2) {
+    const sortedNames = [user1, user2].sort();
+    return `${sortedNames[0]}_${sortedNames[1]}`;
+  }
+
+  socket.on('joinRoom', (data) => {
+    const roomName = getRoomName(data.sender, data.receiver);
+    socket.join(roomName);
+    console.log(`${socket.id} a rejoint la room ${roomName}`);
+  });
+
+  socket.on('leaveRoom', (roomName) => {
+    socket.leave(roomName);
+    console.log(`${socket.id} a quitté la room ${roomName}`);
+  });
+
+  socket.on('chatMessage', (data) => {
+    const roomName = getRoomName(data.sender, data.receiver);
+    io.to(roomName).emit('chatMessage', data);
   });
 
   socket.on('disconnect', () => {
