74e43947ce7548308493a2d7fd8ce9e1eda979ef
Bastien KATUSZYNSKI <katusbastien@gmail.com>
2023-11-15 16:44:39 +0100

    [FEAT] Notification websocket fonctionnel recup ESB to FRONT

--- Notification_Websocket/src/main/java/com/example/Notification_Websocket/ESB/BusListener.java
+++ Notification_Websocket/src/main/java/com/example/Notification_Websocket/ESB/BusListener.java
@@ -1,14 +1,18 @@
 package com.example.Notification_Websocket.ESB;
 
 import com.example.CommonLib.BusNotifModel;
-import com.example.Notification_Websocket.Receiver.Greeting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Message;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 public class BusListener {
@@ -22,7 +26,8 @@ public class BusListener {
     @JmsListener(destination = "NOTIFS", containerFactory = "connectionFactory")
     public void receiveMessage(BusNotifModel busModel, Message message) {
         System.out.println("[BUSLISTENER] [CHANNEL NOTIFS] RECEIVED String MSG=["+message.toString()+"]");
-        // Envoyez un message à la websocket connectée du client
-        messagingTemplate.convertAndSendToUser("Userbonjour", "/topic/greetings", new Greeting("Votre opération est terminée!"));
+        String responseToSend = new SimpleDateFormat("HH:mm").format(new Date()).toString();
+        String topic = "/topic/"+busModel.getTopic();
+        messagingTemplate.convertAndSend(topic, responseToSend);
     }
 }
