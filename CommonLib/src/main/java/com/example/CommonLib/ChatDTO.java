f311016b40f06cb64c99ae9855904d041b73e3f5
Anthony SAINTOT <anthony.saintot@tpg13.cpe.lan>
2023-12-01 14:50:17 +0100

    ChatDTO

--- CommonLib/src/main/java/com/example/CommonLib/ChatDTO.java
+++ CommonLib/src/main/java/com/example/CommonLib/ChatDTO.java
@@ -1,4 +1,71 @@
 package com.example.CommonLib;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ChatDTO {
+    private Integer id;
+    private String message;
+    private int idSender;
+    private int idReceiver;
+    private int numMessage;
+    private int idRoom;
+    public ChatDTO() {
+    }
+
+    public ChatDTO(String message, int idSender, int idReceiver, int numMessage, int idRoom) {
+        this.message = message;
+        this.idSender = idSender;
+        this.idReceiver = idReceiver;
+        this.numMessage = numMessage;
+        this.idRoom = idRoom;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getIdSender() {
+        return idSender;
+    }
+
+    public void setIdSender(int idSender) {
+        this.idSender = idSender;
+    }
+
+    public int getIdReceiver() {
+        return idReceiver;
+    }
+
+    public void setIdReceiver(int idReceiver) {
+        this.idReceiver = idReceiver;
+    }
+
+    public int getIdRoom() {
+        return idRoom;
+    }
+
+    public void setIdRoom(int idRoom) {
+        this.idRoom = idRoom;
+    }
+
+    public int getNumMessage() {
+        return numMessage;
+    }
+
+    public void setNumMessage(int numMessage) {
+        this.numMessage = numMessage;
+    }
 }
